55647a03324c220929c7cb51a7af093960af012e
zhangfurun <122674287@qq.com>
2020-06-12 16:21:48 +0800

    Initial Commit

---
+++ WebPay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebPay
+//
+//  Created by ifenghui on 2020/6/12.
+//  Copyright © 2020 ifenghuI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
